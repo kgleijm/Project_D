f926b08ae8c08fc148af222c38bbedd48f041e9e
Kevin <kevingleijm@gmail.com>
2021-03-22 12:33:28 +0100

    Initial commit

---
+++ Models/genericDataModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project_D.Models
+{
+    public class genericDataModel : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            String conString = ("User ID=postgres;" +
+                                "Password=none;" +
+                                "Host=localhost;" +
+                                "port=5432;" +
+                                "Database=Project_D_Data;" +
+                                "Pooling=true; ");
+            optionsBuilder.UseNpgsql(conString);
+        }
+    }
+}
