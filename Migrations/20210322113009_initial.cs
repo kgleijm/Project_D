b2765be5cbab66df8b06ecb117804ef9d08e6ca3
Lutfi92 <lutf.abed92@gmail.com>
2021-05-09 22:31:27 +0200

    Add project files.

---
+++ Migrations/20210322113009_initial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project_D.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
