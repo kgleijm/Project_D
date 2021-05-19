4b911cf25862e4f5f4a77722f57c405d578009f2
Ben <0984798@hr.nl>
2021-05-19 23:35:49 +0200

    adding data if database is empty
    -adding data if database is empty
    -basic fetching data to admin page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using project_D.Models;
 using Project_D.Models;
 
 namespace Project_D.Controllers
@@ -12,10 +13,62 @@ namespace Project_D.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly project_DContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, project_DContext context)
         {
             _logger = logger;
+            _context = context;
+
+            //check if data is empty
+            if (_context.Department.ToList().Count == 0 && _context.Data.ToList().Count == 0)
+            {
+                //add 2 departments
+                _context.Department.Add( new Department { Name = "Department1" } );
+                _context.Department.Add( new Department { Name = "Department2" } );
+                _context.SaveChanges();
+                //add 2 days of data per department
+                _context.Data.Add(
+                        new Data
+                        {
+                            EnergyConsumption = 20.0f,
+                            GasConsumption = 10.0f,
+                            Date = "18/05/2021",
+                            DepartmentID = _context.Department.ToList()[0].DepartmentID
+                        });
+                _context.Data.Add(
+                        new Data
+                        {
+                            EnergyConsumption = 25.0f,
+                            GasConsumption = 15.0f,
+                            Date = "19/05/2021",
+                            DepartmentID = _context.Department.ToList()[0].DepartmentID
+                        });
+                _context.Data.Add(
+                        new Data
+                        {
+                            EnergyConsumption = 30.0f,
+                            GasConsumption = 20.0f,
+                            Date = "18/05/2021",
+                            DepartmentID = _context.Department.ToList()[1].DepartmentID
+                        });
+                _context.Data.Add(
+                        new Data
+                        {
+                            EnergyConsumption = 35.0f,
+                            GasConsumption = 25.0f,
+                            Date = "19/05/2021",
+                            DepartmentID = _context.Department.ToList()[1].DepartmentID
+                        });
+                _context.SaveChanges();
+            }
+            if (_context.User.ToList().Count == 0)
+            {
+                //add 2 users (1 admin)
+                _context.User.Add( new User { UserName="admin", PassWord="abc123", Email="adminMail@gmail.com", IsAdmin=1 } );
+                _context.User.Add(new User { UserName = "genericUser", PassWord = "", Email = "genericEmail@gmail.com", IsAdmin = 0 });
+                _context.SaveChanges();
+            }
         }
 
         public IActionResult Index()
@@ -38,9 +91,10 @@ namespace Project_D.Controllers
             return View();
         }
 
+        [Route("/Admin")]
         public IActionResult Admin()
         {
-            return View();
+            return View(_context.Data.ToList());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
